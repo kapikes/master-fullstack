d7bcb363c817bb97df5888d6b184fb03ba342c8e
Carlos Delgado <kapikes@gmail.com>
2019-04-10 11:37:10 +0200

    Clase 17.. Uso de Postman, meter datos formulario... OK

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,7 +11,9 @@ class UserController extends Controller
     }
     //creamos dos metodos register y login
     public function register(Request $request){
-        return "Accion de registro de usuario";
+        $name=$request->input('name');
+        $surname=$request->input('surname');
+        return "Accion de registro de usuarios: $name $surname";
     }
     public function login(Request $request){
         return "Accion de login de usuario";
