58afeaa1bab24b9b31478777d7c94305586f88ba
Carlos Delgado <kapikes@gmail.com>
2019-04-24 00:06:01 +0200

    Clase 35.. Creamos un controlador de categorias php artisan rotute:list y te sale todas las rutas que tenemos... pero si en web.php pones Route::resource('/api/category','CategoryController'); TE crean automenticamente TODAS las rutas que necesitas...... OK

--- routes/web.php
+++ routes/web.php
@@ -38,14 +38,17 @@ Route::get('/test-orm', 'PruebasController@testOrm');
 
 //Rutas de API
 //son rutas de pruebas
-Route::get('/usuario/pruebas', 'UserController@pruebas');
-Route::get('/categoria/pruebas', 'CategoryController@pruebas');
-Route::get('/entrada/pruebas', 'PostController@pruebas');
+//Route::get('/usuario/pruebas', 'UserController@pruebas');
+//Route::get('/categoria/pruebas', 'CategoryController@pruebas');
+//Route::get('/entrada/pruebas', 'PostController@pruebas');
 
-//rutas del controlador de usuarios
+//rutas del controlador de USUARIO
 Route::post('/api/register', 'UserController@register');
 Route::post('/api/login', 'UserController@login');
 Route::put('/api/user/update', 'UserController@update');
 Route::post('/api/user/upload', 'UserController@upload')->middleware(App\Http\Middleware\ApiAuthMiddleware::class);
 Route::get('/api/user/avatar/{filename}', 'UserController@getImage');
 Route::get('/api/user/detail/{id}', 'UserController@detail');
+
+//Rutas de controlador de categorias...
+Route::resource('/api/category', 'CategoryController');
