d8e5058c9f545aea0f51a56eabdc0b5ff331f802
Carlos Delgado <kapikes@gmail.com>
2019-04-24 10:30:05 +0200

    Clase 37.. Creamos un metodo nuevo Store para guardar una categoria nueva   OJO muy importante hacer bien el token...... OK

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -8,6 +8,11 @@ use App\Category;
 
 class CategoryController extends Controller {
 
+    //cargamos un constructor utilizamos lo protejemos por autentizacion menos en el index y show
+    public function __construct() {
+        $this->middleware('api.auth', ['except' => ['index', 'show']]);
+    }
+
 //con este metodo sacamos TODAS LAS CATEGORIAS en nuesta bbdd
     public function index() {
         $categories = Category::all();
@@ -18,6 +23,7 @@ class CategoryController extends Controller {
         ]);
     }
 
+//metodo para mostrar una categoria en concreto
     public function show($id) {
         //saco de la bbdd el registro que necesito
         $category = Category::find($id);
@@ -37,4 +43,47 @@ class CategoryController extends Controller {
         return response()->json($data, $data['code']);
     }
 
-}
+    public function store(Request $request) {
+        //Recoger los datos por post
+        $json = $request->input('json', null);
+        $params_array = json_decode($json, true);
+        if (!empty($params_array)) {
+
+
+//Validar los datos
+            $validate = \Validator::make($params_array, [
+                        'name' => 'required'
+            ]);
+            //Guardar la categoria
+            if ($validate->fails()) {
+                $data = [
+                    'code' => 400,
+                    'status' => 'success',
+                    'message' => 'No se ha guardado la categoria'
+                ];
+            } else {
+                $category = new Category();
+                $category->name = $params_array['name'];
+                $category->save();
+
+                $data = [
+                    'code' => 200,
+                    'status' => 'success',
+                    'message' => $category
+                ];
+            }
+        } else {
+            $data = [
+                'code' => 400,
+                'status' => 'error',
+                'message' => 'No has enviado ninguna categoria'
+            ];
+        }
+        //Devolver los resultados
+
+        return response()->json($data, $data['code']);
+    }
+
+    }
+
+
