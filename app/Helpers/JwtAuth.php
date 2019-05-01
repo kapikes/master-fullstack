5cc54ceff24e1636618ad2f82da8b78263f9039e
Carlos Delgado <kapikes@gmail.com>
2019-05-01 15:28:08 +0200

    Clase 40.. Creamos un metodo para guardar un POST NO ESTOY SEGURO DE QUE ESTE BIEN   OJO ......

--- app/Helpers/JwtAuth.php
+++ app/Helpers/JwtAuth.php
@@ -1,74 +1,89 @@
 <?php
 namespace App\Helpers;
-
+ 
 use Firebase\JWT\JWT;
 use Illuminate\Support\Facades\DB;
 use App\User;
-
+ 
 class JwtAuth{
+    
     public $key;
-    public function __construct(){
-        $this->key ='Esto es una clave super secreta';
-    }
-    public function signup($email, $password, $getToken=null){
     
-    //Buscar si existe el usuario con sus credenciales...
-    $user=User::where([
-        'email'=>$email,
-        'password'=>$password
-    ])->first();
+    public function __construct() {
+        $this->key = 'esto_es_una_clave_super_secreta-99887766';
+    }
     
-//Comprobar si son correctas
-    $signup=false;
-    if(is_object($user)){
-        $signup=true;
-    }
-    //Generar el token con los datos del usuario
-if($signup){
-    $token=array(
-        'sub'       =>      $user->id,
-        'email'     =>      $user->email,  
-        'name'      =>      $user->name,
-        'surname'   =>      $user->surname,
-        'iat'       =>      time(),
-        'exp'       =>      time() + (7 * 24 * 60 * 60)
-    );
-    $jwt=JWT::encode($token, $this->key, 'HS256');
-    $decoded= JWT::decode($jwt, $this->key, ['HS256']);
-    //Devolver los datos decodificados o el token, en funcion de un parametrto
-    if(is_null($getToken)){
-        $data = $jwt;
-    }else{
-        $data= $decoded;
-    }
-}else{
-    $data=array(
-        'status'=>'error',
-        'message'=>'Login Incorrecto'
-    );
-}    
-
-      
+    public function signup($email, $password, $getToken = null){
+        
+        // Buscar si existe el usuario con sus credenciales
+         $user = User::where([
+                'email' => $email,
+                'password' => $password
+         ])->first();
+         
+        // Comprobar si son correctas(objeto)
+        $signup = false;
+        if(is_object($user)){
+            $signup = true;
+        }
+        
+        // Generar el token con los datos del usuario idenficado
+        if($signup){
+            
+            $token = array(
+              'sub'     =>      $user->id,
+              'email'   =>      $user->email,
+              'name'    =>      $user->name,
+              'surname' =>      $user->surname,
+              'description'  => $user->description,
+              'image'   =>      $user->image,
+              'iat'     =>      time(),
+              'exp'     =>      time() + (7 * 24 * 60 * 60)
+            );
+            
+            $jwt = JWT::encode($token, $this->key, 'HS256');
+            $decoded = JWT::decode($jwt, $this->key, ['HS256']);
+            
+            // Devoler los datos decodificados o el token, en función de un parametro
+            if(is_null($getToken)){
+                $data = $jwt;
+            }else{
+                $data = $decoded;
+            }
+            
+        }else{
+            $data = array(
+                'status' => 'error',
+                'message' => 'Login incorrecto.'
+            );
+        }
+        
         return $data;
     }
-     public function checkToken($jwt, $getIdentity=false){
-         $auth=false;
-         try{
-             $jwt=str_replace('"',"",$jwt);
-             $decoded=JWT::decode($jwt, $this->key, ['HS256']);
-     }catch(\UnexpectedValueException $e){
-         $auth=false;
-     }catch(\DomainException $e){
-         $auth=false;
-     }
-     if(!empty($decoded)&& is_object($decoded) && isset($decoded->sub)){
-         $auth=true;
-     }else{
-         $auth=false;
-     }
-     if($getIdentity){
-         return $decoded;
-     }
-     return $auth;
-     }
+    
+    public function checkToken($jwt, $getIdentity = false){
+        $auth = false;
+        
+        try{
+            $jwt = str_replace('"', '', $jwt);
+            $decoded = JWT::decode($jwt, $this->key, ['HS256']);
+        }catch(\UnexpectedValueException $e){
+            $auth = false;
+        }catch(\DomainException $e){
+            $auth = false;
+        }
+        
+        if(!empty($decoded) && is_object($decoded) && isset($decoded->sub)){
+            $auth = true;  
+        }else{
+            $auth = false;
+        }
+        
+        if($getIdentity){
+            return $decoded;
+        }
+        
+        return $auth;
+    }
+    
 }
--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,41 +5,99 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Post;
+use App\Helpers\JwtAuth;
 
-class PostController extends Controller
-{
-     //cargamos un constructor utilizamos lo protejemos por autentizacion menos en el index y show
+class PostController extends Controller {
+
+    //cargamos un constructor utilizamos lo protejemos por autentizacion menos en el index y show
     public function __construct() {
         $this->middleware('api.auth', ['except' => ['index', 'show']]);
     }
-   //Metodo en el que nos saca un listado de TODAS las entradas que necesitamos....
-    public function index(){
-        $post= Post::all()->load('category');
+
+    //Metodo en el que nos saca un listado de TODAS las entradas que necesitamos....
+    public function index() {
+        $post = Post::all()->load('category');
         return response()->json([
-            'code'=>200,
-            'status'=>'success',
-            'post'=>$post
-        ],200);
+                    'code' => 200,
+                    'status' => 'success',
+                    'post' => $post
+                        ], 200);
     }
+
     //Metodo para sacarnos el registro que nosotros queramos DETALLE
-    public function show($id){
-        $post=Post::find($id);
-        if(is_object($post)){
-            $data=[
-            'code'=>200,
-            'status'=>'success',
-            'post'=>$post
-        ];
-            }else{
-               $data=[
-            'code'=>404,
-            'status'=>'error',
-            'message'=>'La entrada NO EXISTE'
-        ]; 
-            }
-            
-            
-           return response()->json($data, $data['code']); 
-        
+    public function show($id) {
+        $post = Post::find($id);
+        if (is_object($post)) {
+            $data = [
+                'code' => 200,
+                'status' => 'success',
+                'post' => $post
+            ];
+        } else {
+            $data = [
+                'code' => 404,
+                'status' => 'error',
+                'message' => 'La entrada NO EXISTE'
+            ];
+        }
+
+
+        return response()->json($data, $data['code']);
     }
+
+    public function store(Request $request) {
+        //Recoger los datos por post
+        $json = $request->input('json', null);
+        $params = json_decode($json);
+        $params_array = json_decode($json, true);
+        
+        if (!empty($params_array)) {
+            //Conseguir usuario identificado
+            $jwtAuth = new JwtAuth();
+            $token = $request->header('Autorization', null);
+            $user = $jwtAuth->checkToken($token, true);
+
+            //Validar los datos
+            $validate = \Validator::make($params_array, [
+                        'title' => 'required',
+                        'content' => 'required',
+                        'category_id' => 'required',
+                        'image' => 'required'
+            ]);
+            //Guardar la categoria
+            if ($validate->fails()) {
+                $data = [
+                    'code' => 400,
+                    'status' => 'success',
+                    'message' => 'No se ha guardado el post... FALTAN DATOS'
+                ];
+            } else {
+//                //Guardar el articulo
+                $post = new Post();
+                $post->user_id = $user->sub;
+                $post->category_id = $params->category_id;
+                $post->title = $params->title;
+                $post->content = $params->content;
+                $post->image = $params->image;
+                $post->save();
+
+
+                $data = [
+                    'code' => 200,
+                    'status' => 'success',
+                    'post' => $post
+                ];
+            }
+        } else {
+            $data = [
+                'code' => 400,
+                'status' => 'error',
+                'message' => 'Envia los datos correctamente'
+            ];
+        }
+        //Devolver los resultados
+
+        return response()->json($data, $data['code']);
+    }
+
 }
